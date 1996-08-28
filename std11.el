3511c93516fecf8ce6ef1f532329b7795819b574
morioka <morioka>
1996-08-28 20:33:54 +0000

    (std11-parse-address, std11-parse-addresses): Add as autoload function.

--- std11.el
+++ std11.el
@@ -179,6 +179,8 @@ If BOUNDARY is not nil, it is used as message header separator.
 
 (provide 'std11)
 
-(autoload 'std11-lexical-analyze "std11-parse")
+(autoload 'std11-lexical-analyze	"std11-parse")
+(autoload 'std11-parse-address		"std11-parse")
+(autoload 'std11-parse-addresses	"std11-parse")
 
 ;;; std11.el ends here
