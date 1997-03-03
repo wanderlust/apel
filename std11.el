74ac43b566320cf388e90da1beea1b6fb231a04e
shuhei-k <shuhei-k>
1997-03-03 08:03:06 +0000

    (std11-addr-to-string): Enclose quoted-string with double-quote.

--- std11.el
+++ std11.el
@@ -1,6 +1,6 @@
 ;;; std11.el --- STD 11 functions for GNU Emacs
 
-;; Copyright (C) 1995,1996 Free Software Foundation, Inc.
+;; Copyright (C) 1995,1996,1997 Free Software Foundation, Inc.
 
 ;; Author:   MORIOKA Tomohiko <morioka@jaist.ac.jp>
 ;; Keywords: mail, news, RFC 822, STD 11
@@ -263,13 +263,14 @@ If BOUNDARY is not nil, it is used as message header separator.
 represents addr-spec of RFC 822. [std11.el]"
   (mapconcat (function
 	      (lambda (token)
-		(if (let ((name (car token)))
-		      (or (eq name 'spaces)
-			  (eq name 'comment)
-			  ))
-		    ""
-		  (cdr token)
-		  )))
+		(let ((name (car token)))
+                  (cond
+                   ((eq name 'spaces) "")
+                   ((eq name 'comment) "")
+                   ((eq name 'quoted-string)
+                    (concat "\"" (cdr token) "\""))
+                   (t (cdr token)))
+                  )))
 	     seq "")
   )
 
