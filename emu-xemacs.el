312d93b8d5592959ee878adde4584f549512fd18
morioka <morioka>
1998-03-09 07:34:43 +0000

    tm 6.80.

--- emu-xemacs.el
+++ emu-xemacs.el
@@ -16,23 +16,24 @@
 (or (face-differs-from-default-p 'underline)
     (set-face-underline-p 'underline t))
 
-(or (fboundp 'set-text-properties)
-    (defun set-text-properties (start end props &optional buffer)
-      (if (or (null buffer) (bufferp buffer))
-	  (if props
-	      (while props
-		(put-text-property 
-		 start end (car props) (nth 1 props) buffer)
-		(setq props (nthcdr 2 props)))
-	    (remove-text-properties start end ())
-	    )))
-    )
-
-(defalias 'make-overlay 'make-extent)
-(defalias 'overlay-put 'set-extent-property)
-
-(defun move-overlay (extent start end &optional buffer)
-  (set-extent-endpoints extent start end)
-  )
+;;; (or (fboundp 'set-text-properties)
+;;;     (defun set-text-properties (start end props &optional buffer)
+;;;       (if (or (null buffer) (bufferp buffer))
+;;;           (if props
+;;;               (while props
+;;;                 (put-text-property 
+;;;                  start end (car props) (nth 1 props) buffer)
+;;;                 (setq props (nthcdr 2 props)))
+;;;             (remove-text-properties start end ())
+;;;             )))
+;;;     )
+;;; 
+;;; (defalias 'make-overlay 'make-extent)
+;;; (defalias 'overlay-put 'set-extent-property)
+;;; (defalias 'overlay-buffer 'extent-buffer)
+;;; 
+;;; (defun move-overlay (extent start end &optional buffer)
+;;;   (set-extent-endpoints extent start end)
+;;;   )
 
 (provide 'emu-xemacs)
