23901242dd72ad26a2975cc261c8437a4c6ab3c4
morioka <morioka>
1998-04-13 13:30:14 +0000

    (install-detect-elisp-directory): Modify regexp to allow trailing `/'.

--- install.el
+++ install.el
@@ -3,11 +3,11 @@
 ;; Copyright (C) 1996 Free Software Foundation, Inc.
 
 ;; Author: MORIOKA Tomohiko <morioka@jaist.ac.jp>
-;; Created: 1996/8/18
+;; Created: 1996/08/18
 ;; Version: $Id$
-;; Keywords: install
+;; Keywords: install, byte-compile, directory detection
 
-;; This file is part of tl (Tiny Library).
+;; This file is part of APEL (A Portable Emacs Library).
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -20,11 +20,18 @@
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
+;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
 ;;; Code:
+
+(require 'emu)
+(require 'path-util)
+
+
+;;; @ compile Emacs Lisp files
+;;;
 
 (defun compile-elisp-module (module &optional path every-time)
   (setq module (expand-file-name (symbol-name module) path))
@@ -45,6 +52,9 @@
 	  modules))
 
 
+;;; @ install files
+;;;
+
 (defvar install-overwritten-file-modes (+ (* 64 6)(* 8 4) 4))
 
 (defun install-file (file src dest &optional move overwrite)
@@ -52,12 +62,13 @@
     (if (file-exists-p src-file)
 	(let ((full-path (expand-file-name file dest)))
 	  (if (and (file-exists-p full-path) overwrite)
-	      (set-file-modes full-path install-overwritten-file-modes)
+              (delete-file full-path)
 	    )
 	  (copy-file src-file full-path t t)
 	  (if move
 	      (catch 'tag
-		(while (file-exists-p src-file)
+		(while (and (file-exists-p src-file)
+			    (file-writable-p src-file))
 		  (condition-case err
 		      (progn
 			(delete-file src-file)
@@ -78,6 +89,10 @@
 		      ))
 	  files))
 
+
+;;; @@ install Emacs Lisp files
+;;;
+
 (defun install-elisp-module (module src dest)
   (let (el-file elc-file)
     (let ((name (symbol-name module)))
@@ -88,7 +103,7 @@
       (if (file-exists-p src-file)
 	  (let ((full-path (expand-file-name el-file dest)))
 	    (if (file-exists-p full-path)
-		(set-file-modes full-path install-overwritten-file-modes)
+                (delete-file full-path)
 	      )
 	    (copy-file src-file full-path t t)
 	    (princ (format "%s -> %s\n" el-file dest))
@@ -96,6 +111,9 @@
       (setq src-file (expand-file-name elc-file src))
       (if (file-exists-p src-file)
 	  (let ((full-path (expand-file-name elc-file dest)))
+            (if (file-exists-p full-path)
+                (delete-file full-path)
+              )
 	    (copy-file src-file full-path t t)
 	    (catch 'tag
 	      (while (file-exists-p src-file)
@@ -118,6 +136,65 @@
 		      (install-elisp-module module src dest)
 		      ))
 	  modules))
+
+
+;;; @ detect install path
+;;;
+
+(defvar install-prefix
+  (if (or running-emacs-18 running-xemacs)
+      (expand-file-name "../../.." exec-directory)
+    (expand-file-name "../../../.." data-directory)
+    )) ; install to shared directory (maybe "/usr/local")
+
+(defvar install-elisp-prefix
+  (if (>= emacs-major-version 19)
+      "site-lisp"
+    "local.lisp"))
+
+(defun install-detect-elisp-directory (&optional prefix elisp-prefix
+						 allow-version-specific)
+  (or prefix
+      (setq prefix install-prefix)
+      )
+  (or elisp-prefix
+      (setq elisp-prefix install-elisp-prefix)
+      )
+  (or
+   (catch 'tag
+     (let ((rest default-load-path)
+	   dir)
+       (while (setq dir (car rest))
+	 (if (string-match
+	      `,(concat "^"
+			(expand-file-name (concat ".*/" elisp-prefix) prefix)
+			"/?$")
+	      dir)
+	     (if (or allow-version-specific
+		     (not (string-match (format "%d\\.%d"
+						emacs-major-version
+						emacs-minor-version) dir))
+		     )
+		 (throw 'tag dir)
+	       ))
+	 (setq rest (cdr rest))
+	 )))
+   (expand-file-name (concat
+		      (if running-emacs-19_29-or-later
+			  "share/"
+			"lib/")
+		      (cond ((boundp 'NEMACS) "nemacs/")
+			    ((boundp 'MULE)   "mule/")
+			    (running-xemacs
+			     (if (featurep 'mule)
+				 "xmule/"
+			       "xemacs/"))
+			    (t "emacs/"))
+		      elisp-prefix) prefix)
+   ))
+
+(defvar install-default-elisp-directory
+  (install-detect-elisp-directory))
 
 
 ;;; @ end
