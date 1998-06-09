ab0dbfb9bc02c48bb269c5609299d95cab1ebb04
yamaoka <yamaoka>
1998-06-09 01:43:30 +0000

    emu.el (split-string) (with-temp-buffer) (with-current-buffer) (save-current-buffer): New function and macros for Emacs 19,18.

--- emu.el
+++ emu.el
@@ -333,6 +333,54 @@ This function does not move point. [XEmacs emulating function]"
     )
 
 
+;;; @ for Emacs 19,18
+;;;
+
+(defmacro-maybe save-current-buffer (&rest body)
+  "Save the current buffer; execute BODY; restore the current buffer.
+Executes BODY just like `progn'."
+  (` (let ((orig-buffer (current-buffer)))
+       (unwind-protect
+	   (progn (,@ body))
+	 (set-buffer orig-buffer)))))
+
+;; This macro was imported XEmacs 21.
+(defmacro-maybe with-current-buffer (buffer &rest body)
+  "Execute the forms in BODY with BUFFER as the current buffer.
+The value returned is the value of the last form in BODY.
+See also `with-temp-buffer'."
+  (` (save-current-buffer
+       (set-buffer (, buffer))
+       (,@ body))))
+
+;; This macro was imported XEmacs 21.
+(defmacro-maybe with-temp-buffer (&rest forms)
+  "Create a temporary buffer, and evaluate FORMS there like `progn'.
+See also `with-temp-file' and `with-output-to-string'."
+  (let ((temp-buffer (make-symbol "temp-buffer")))
+    (` (let (((, temp-buffer)
+	      (get-buffer-create (generate-new-buffer-name " *temp*"))))
+	 (unwind-protect
+	     (with-current-buffer (, temp-buffer)
+	       (,@ forms))
+	   (and (buffer-name (, temp-buffer))
+		(kill-buffer (, temp-buffer))))))))
+
+;; This function was imported from XEmacs 21.
+(defun-maybe split-string (string &optional pattern)
+  "Return a list of substrings of STRING which are separated by PATTERN.
+If PATTERN is omitted, it defaults to \"[ \\f\\t\\n\\r\\v]+\"."
+  (or pattern
+      (setq pattern "[ \f\t\n\r\v]+"))
+  ;; The FSF version of this function takes care not to cons in case
+  ;; of infloop.  Maybe we should synch?
+  (let (parts (start 0))
+    (while (string-match pattern string start)
+      (setq parts (cons (substring string start (match-beginning 0)) parts)
+	    start (match-end 0)))
+    (nreverse (cons (substring string start) parts))))
+
+
 ;;; @ for text/richtext and text/enriched
 ;;;
 
