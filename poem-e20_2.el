1dd02ce62400d95e92d0a8747f163804da07e52e
morioka <morioka>
1998-09-17 16:27:53 +0000

    (set-buffer-multibyte): Use `defun-maybe' instead of `defsubst-maybe'.

--- poem-e20_2.el
+++ poem-e20_2.el
@@ -31,7 +31,7 @@
 ;;; @ buffer representation
 ;;;
 
-(defsubst-maybe set-buffer-multibyte (flag)
+(defun-maybe set-buffer-multibyte (flag)
   "Set the multibyte flag of the current buffer to FLAG.
 If FLAG is t, this makes the buffer a multibyte buffer.
 If FLAG is nil, this makes the buffer a single-byte buffer.
