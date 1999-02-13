19469cd555da6053c99f73b30a2889016e836916
akr <akr>
1999-02-13 08:23:27 +0000

    * mcs-e20.el (charsets-mime-charset-alist): Don't set up `iso-2022-int-1' in default.

--- mcs-e20.el
+++ mcs-e20.el
@@ -85,20 +85,20 @@
 	    latin-jisx0201 japanese-jisx0208-1978
 	    chinese-gb2312 japanese-jisx0208
 	    korean-ksc5601 japanese-jisx0212)		. iso-2022-jp-2)
-    ((ascii latin-iso8859-1 greek-iso8859-7
-	    latin-jisx0201 japanese-jisx0208-1978
-	    chinese-gb2312 japanese-jisx0208
-	    korean-ksc5601 japanese-jisx0212
-	    chinese-cns11643-1 chinese-cns11643-2)	. iso-2022-int-1)
-    ((ascii latin-iso8859-1 latin-iso8859-2
-	    cyrillic-iso8859-5 greek-iso8859-7
-	    latin-jisx0201 japanese-jisx0208-1978
-	    chinese-gb2312 japanese-jisx0208
-	    korean-ksc5601 japanese-jisx0212
-	    chinese-cns11643-1 chinese-cns11643-2
-	    chinese-cns11643-3 chinese-cns11643-4
-	    chinese-cns11643-5 chinese-cns11643-6
-	    chinese-cns11643-7)				. iso-2022-int-1)
+;     ((ascii latin-iso8859-1 greek-iso8859-7
+; 	    latin-jisx0201 japanese-jisx0208-1978
+; 	    chinese-gb2312 japanese-jisx0208
+; 	    korean-ksc5601 japanese-jisx0212
+; 	    chinese-cns11643-1 chinese-cns11643-2)	. iso-2022-int-1)
+;     ((ascii latin-iso8859-1 latin-iso8859-2
+; 	    cyrillic-iso8859-5 greek-iso8859-7
+; 	    latin-jisx0201 japanese-jisx0208-1978
+; 	    chinese-gb2312 japanese-jisx0208
+; 	    korean-ksc5601 japanese-jisx0212
+; 	    chinese-cns11643-1 chinese-cns11643-2
+; 	    chinese-cns11643-3 chinese-cns11643-4
+; 	    chinese-cns11643-5 chinese-cns11643-6
+; 	    chinese-cns11643-7)				. iso-2022-int-1)
     ))
 
 
